83fc586976327f6b1d02585a887cb0b21e428a6e
danymcboockair <dsmmgc@gmail.com>
2024-07-31 19:05:34 +0100

    Arrays 01 ok

--- Arrays/Arrays_01.java
+++ Arrays/Arrays_01.java
@@ -1,9 +1,23 @@
+package Arrays;
+
 /*Ejercicio Array 1
 Leer 5 números y mostrarlos en el mismo orden introducido*/
-package Arrays;
+
+import java.util.Scanner;
 
 public class Arrays_01 {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
+        int[] num = new int[5];
+
+        for (int i = 0; i < num.length; i++) {
+            num[i] = sc.nextInt();
+        }
+        for (int i = 0; i < num.length; i++) {
+            num[i] = i;
+            System.out.print(num[i] + ", ");
+        }
     }
 }
