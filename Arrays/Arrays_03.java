ce61f27373438d42de0f98f56100aaee1bd24aa1
danymcboockair <dsmmgc@gmail.com>
2024-07-31 23:38:24 +0100

    Arrays 03

--- Arrays/Arrays_03.java
+++ Arrays/Arrays_03.java
@@ -4,8 +4,45 @@ de los negativos y contar el número de ceros.*/
 
 package Arrays;
 
+import java.util.Scanner;
+
 public class Arrays_03 {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
+        int[] num = new int[5];
+        int mediaPositivos = 0;
+        int mediaNegagtivos = 0;
+        int ceros = 0;
+        int numeroCeros = 0;
+
+        for (int i = 0; i < num.length; i++) {
+            num[i] = sc.nextInt();
+        }
+        for (int i = 0; i < num.length; i++) {
+            int positivos = num[i];
+            if (positivos > 0) {
+                mediaPositivos += positivos;
+            }
+        }
+        for (int i = 0; i < num.length; i++) {
+            int negativos = num[i];
+            if (negativos < 0) {
+                mediaNegagtivos += negativos;
+            }
+        }
+        if (mediaPositivos > 0) {
+            System.out.println("La media de números positivos es: " + mediaPositivos / num.length);
+        }
+        if (mediaNegagtivos < 0) {
+            System.out.println("La media de números negativos es: " + mediaNegagtivos / num.length);
+        }
+        for (int i = 0; i < num.length; i++) {
+            if (num[i] == ceros) {
+                numeroCeros++;
+            }
+        }
+        System.out.println("El número de ceros es: " + numeroCeros);
     }
 }
--- out/production/Josema_Java/Arrays/Readme.md
+++ out/production/Josema_Java/Arrays/Readme.md
@@ -11,7 +11,7 @@ Vamos con los siguientes ejercicios:
 Ejercicio Array 1 ✅
 Leer 5 números y mostrarlos en el mismo orden introducido.
 
-Ejercicio Array 2
+Ejercicio Array 2 ✅
 Leer 5 números y mostrarlos en orden inverso al introducido.
 
 Ejercicio Array 3
