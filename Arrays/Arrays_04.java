dcda8b39390109904a02332a12d3d0fb7f948f98
danymcbookair <dsmmgc@gmail.com>
2024-08-01 22:26:01 +0100

    avance 05

--- Arrays/Arrays_04.java
+++ Arrays/Arrays_04.java
@@ -24,3 +24,4 @@ public class Arrays_04 {
 
     }
 }
+// ok
--- Arrays/Arrays_05.java
+++ Arrays/Arrays_05.java
@@ -2,9 +2,30 @@
 Leer por teclado dos tablas de 10 números enteros y mezclarlas en una tercera de la forma: el 1º de A, el 1º
         de B, el 2º de A, el 2º de B, etc.*/
 package Arrays;
+import java.util.Scanner;
 
 public class Arrays_05 {
     public static void main(String[] args) {
 
+        //Pendiente verificar, error de Exception
+
+        Scanner sc = new Scanner(System.in);
+        int[] numA = new int[10];
+        int[] numB = new int[10];
+        int[] numC = new int[numA.length + numB.length];
+
+        for (int i = 0; i < numA.length; i++) {
+            numA[i] = sc.nextInt();
+        }
+        for (int i = 0; i < numB.length; i++) {
+            numB[i] = sc.nextInt();
+        }
+        for (int i = 0; i < numC.length; i++) {
+            numC[i] = numA[i];
+            System.out.print(numC[i] + ", ");
+            numC[i] = numB[i];
+            System.out.print(numC[i]);
+            System.out.println(" ");
+        }
     }
 }
