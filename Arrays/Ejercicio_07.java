91365080787bc671f668b65c506f4b0fe22adc0f
danymcboockair <dsmmgc@gmail.com>
2024-08-03 01:45:36 +0100

    Añadidos enunciados

--- Arrays/Ejercicio_07.java
+++ Arrays/Ejercicio_07.java
@@ -1,4 +1,9 @@
 package Arrays;
 
+/*Ejercicio Array 7
+Crea un array de números donde le indicamos por teclado el tamaño del array,
+rellenaremos el array con números aleatorios entre 0 y 9, al final muestra por
+pantalla el valor de cada posición y la suma de todos los valores*/
+
 public class Ejercicio_07 {
 }
--- Arrays/Ejercicio_10.java
+++ Arrays/Ejercicio_10.java
@@ -1,4 +1,11 @@
 package Arrays;
 
+/*Ejercicio Array 10 Define un array de 12 números enteros con nombre num y asigna los valores según la tabla que
+ se muestra a continuación. Muestra el contenido de todos los elementos del array. ¿Qué sucede con los valores de
+ los elementos que no han sido inicializados?
+
+Posición 0 1 2 3 4 5 6 7 8 9 10 11
+Valor 39 -2 0 14 5 120*/
+
 public class Ejercicio_10 {
 }
